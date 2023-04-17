ec814da682aab6140ed90ef2269e131a517ca239
frontend_dmytro_f <dmytro.fedchenko@abz.agency>
2023-04-18 01:42:15 +0300

    feat: implement main page

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
   },
   plugins: ['react', 'prettier', '@typescript-eslint'],
   rules: {
-    allowImportingTsExtensions: 'off',
     'react/display-name': 'off',
     'no-throw-literal': 'off',
     'no-return-assign': 'off',
