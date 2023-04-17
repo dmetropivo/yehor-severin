04d24e01d0aec75174a5eab9145f7bbf2ec876fc
frontend_dmytro_f <dmytro.fedchenko@abz.agency>
2023-04-18 01:26:04 +0300

    feat: implement main page

--- src/UI/layouts/MainPageLayout/MainPageLayout.tsx
+++ src/UI/layouts/MainPageLayout/MainPageLayout.tsx
@@ -1,7 +1,12 @@
 import NavMenu from '@/UI/menus/NavMenu/NavMenu';
 import styles from './MainPageLayout.module.scss';
+import { FC, ReactNode } from 'react';
 
-const MainPageLayout = ({ children }) => {
+interface IMainPageLayout {
+  children: ReactNode;
+}
+
+const MainPageLayout: FC<IMainPageLayout> = ({ children }) => {
   return (
     <div className={'mainPageLayout'}>
       <main className={`${styles.main} container`}>
